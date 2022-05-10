f554a775a3a06289ecacc7d9379df33f27962d57
Xsarz <88231084+xXxCLOTIxXx@users.noreply.github.com>
2022-05-10 17:29:49 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace colculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число");
+            string num1 = Console.ReadLine();
+            Console.WriteLine("Введите символ (+ - * :)");
+            string symbol = Console.ReadLine();
+            Console.WriteLine("Введите второе число");
+            string num2 = Console.ReadLine();
+            if (symbol == "+")
+            {
+                Console.WriteLine($"Ответ: {Convert.ToInt32(num1) + Convert.ToInt32(num2)}");
+            }
+            else if (symbol == "-")
+            {
+                Console.WriteLine($"Ответ: {Convert.ToInt32(num1) - Convert.ToInt32(num2)}");
+            }
+            else if (symbol == "*")
+            {
+                Console.WriteLine($"Ответ: {Convert.ToInt32(num1) * Convert.ToInt32(num2)}");
+            }
+            else if (symbol == ":")
+            {
+                Console.WriteLine($"Ответ: {Convert.ToInt32(num1) : Convert.ToInt32(num2)}");
+            }
+            else
+            {
+                Console.WriteLine("Вы ввели неверный символ!");
+            }
+
+        }
+    }
+}
